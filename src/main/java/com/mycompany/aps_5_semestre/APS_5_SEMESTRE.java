7cb1f146cfa23ccd79a4f51451563c4ca6109fe4
Caio Arantes <caio.arantes05@gmail.com>
2024-03-11 20:37:09 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/aps_5_semestre/APS_5_SEMESTRE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aps_5_semestre;
+
+/**
+ *
+ * @author caio
+ */
+public class APS_5_SEMESTRE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
